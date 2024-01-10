19b7800120824bce1c3663c12b97749d5b87aa8b
Raphael Beaudet <raphaelbeaudet@icloud.com>
2024-01-10 23:05:16 +0100

    favicon

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,8 @@
                 <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div class="flex lg:flex-1">
                     <NuxtLink to="/" class="-m-1.5 p-1.5">
-                    <span class="sr-only">Arvy</span>
-                    <img class="h-8 w-auto" src="/arvy-logo.png" alt="">
+                        <span class="sr-only">Arvy</span>
+                        <img class="h-8 w-auto" src="/arvy-logo.png" alt="">
                     </NuxtLink>
                 </div>
                 <div class="flex lg:hidden">
